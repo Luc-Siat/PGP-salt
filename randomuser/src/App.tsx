4012e4e35a6b563f9684f297a44af4695ce6c1ba
luc siat <lucsiat@gmail.com>
2023-05-08 14:10:17 +0200

    adding formik

--- randomuser/src/App.tsx
+++ randomuser/src/App.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react'
+import { SyntheticEvent, useEffect, useState } from 'react'
 import './App.css'
 import { getUser } from './services/api';
 import { IUser } from './interfaces/interfaces';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 function App() {
 
@@ -9,7 +10,15 @@ function App() {
 
   const getData = async () => {
     setUser(await getUser());
-    console.log(await getUser());
+  }
+
+  const updateName = (e: SyntheticEvent, name : string) => {
+    const newUser : IUser = {
+      name: name,
+      address: user!.address,
+      age: user!.age
+    }
+    setUser(newUser);
   }
 
   useEffect(() => {
@@ -21,6 +30,42 @@ function App() {
       <h3>{user?.name}</h3>
       <p>{user?.address}</p>
       <p>{user?.age} years old</p>
+      <br />
+      <div>
+     <h1>Any place in your app!</h1>
+     <Formik
+       initialValues={{ email: '', password: '' }}
+       validate={values => {
+         const errors = {};
+         if (!values.email) {
+           errors.email = 'Required';
+         } else if (
+           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+         ) {
+           errors.email = 'Invalid email address';
+         }
+         return errors;
+       }}
+       onSubmit={(values, { setSubmitting }) => {
+         setTimeout(() => {
+           alert(JSON.stringify(values, null, 2));
+           setSubmitting(false);
+         }, 400);
+       }}
+     >
+       {({ isSubmitting }) => (
+         <Form>
+           <Field type="email" name="email" />
+           <ErrorMessage name="email" component="div" />
+           <Field type="password" name="password" />
+           <ErrorMessage name="password" component="div" />
+           <button type="submit" disabled={isSubmitting}>
+             Submit
+           </button>
+         </Form>
+       )}
+     </Formik>
+   </div>
     </>
   )
 }
